f67a5b7a04e2ecf3546aa3862b7efe62a33fcec3
David Silva <zgrav@null.net>
2018-03-31 11:05:01 +0200

    feat(app): control white LED on pktchip

--- main_process.js
+++ main_process.js
@@ -2,6 +2,28 @@
 const electron = require('electron')
 const {app, BrowserWindow} = electron
 
+const sudo = require('sudo-prompt');
+const sudoOptions = {
+  name: 'Electron',
+};
+
+const PKTCHIP_KRNL = '4.4.13-ntc-mlc';
+const PKTCHIP_ARCH = 'armv7l';
+
+const exec = require('child_process').exec;
+let arch;
+let krnl;
+
+exec('uname -m', function (error, stdout, stderr) {
+  if (error) throw error;
+  arch = stdout;
+});
+
+exec('uname -r', function (error, stdout, stderr) {
+  if (error) throw error;
+  krnl = stdout;
+});
+
 // Let electron reloads by itself when webpack watches changes in ./app/
 require('electron-reload')(__dirname)
 
@@ -9,9 +31,29 @@ require('electron-reload')(__dirname)
 let mainWindow
 
 app.on('ready', () => {
+    if (arch === PKTCHIP_ARCH && krnl === PKTCHIP_KRNL) {
+      sudo.exec('echo heartbeat | tee /sys/class/leds/chip\:white\:status/trigger > /dev/null', sudoOptions,
+        function(error, stdout, stderr) {
+          if (error) throw error;
+          console.log('stdout: ' + stdout);
+        }
+      );
+    }
 
     mainWindow = new BrowserWindow({width: 480, height: 272})
 
     mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 
+})
+
+app.on('before-quit',function()
+{
+  if (arch === PKTCHIP_ARCH && krnl === PKTCHIP_KRNL) {
+    sudo.exec('echo none | tee /sys/class/leds/chip\:white\:status/trigger > /dev/null', sudoOptions,
+      function(error, stdout, stderr) {
+        if (error) throw error;
+        console.log('stdout: ' + stdout);
+      }
+    );
+  }
 })
